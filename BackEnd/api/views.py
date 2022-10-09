1e66f5ea9801d9cdc73f4423cfbe276a278183ae
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-10-09 19:19:00 -0400

    Adding endpoints and database modifications

--- BackEnd/api/views.py
+++ BackEnd/api/views.py
@@ -1,69 +1,114 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
-from .serializers import Vacante_Serializer, Empresa_Serializer, Solicitude_Serializer
+from .serializers import Candidato_Serializer, Vacante_Serializer, Empresa_Serializer, Solicitude_Serializer
 
-from vacantes.models import Vacante
+from vacantes.models import Vacante, Solicitude
+
 # Create your views here.
+class ApiView(APIView):
 
-@api_view(['GET'])
-def api(request):
+    def get(self, request, *args, **kwargs):
+            
+        api_urls = {
+            'token': 'api/token',
+            'token-refresh': 'api/token/refresh',
+            'crear-vacante': 'api/vacantes',
+            'crear-empresa': 'api/crear/empresa',
+            'obtener-vacantes': 'api/vacantes',
+            'obtener-vacante': 'api/obtener/vacante/id',
+            'crear-solicitud': 'api/crear/solicitud/',
+            'solicitudes-vacante': 'api/solicitudes/vacante/id',
+            'vacantes-empresa': 'api/vacantes/empresa/id',
+        }
 
-    api_urls = {
-        'token': 'api/token',
-        'token-refresh': 'api/token/refresh',
-        'crear-vacante': 'api/crear/vacante',
-        'crear-empresa': 'api/crear/empresa',
-        'obtener-vacantes': 'api/obtener/vacantes',
-        'obtener-vacante': 'api/obtener/vacante/',
-        'crear-solicitud': 'api/crear/solicitud/',
-    }
+        return Response(api_urls)
 
-    return Response(api_urls)
+class VacantesView(APIView):
+    serializer_class = Vacante_Serializer
+    
+    def get(self, request, *args, **kwargs):
+        vacantes = Vacante.objects.all()
+        serializer = self.serializer_class(vacantes, many=True)
 
-@api_view(['POST'])
-def crear_vacante(request):
-
-    serializer = Vacante_Serializer(data=request.data)
-
-    if serializer.is_valid():
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
 
-    return Response(serializer.data)
+        return Response(serializer.data)
 
-@api_view(['POST'])
-def crear_empresa(request):
+class EmpresaView(APIView):
 
-    serializer = Empresa_Serializer(data=request.data)
+    serializer_class = Empresa_Serializer
 
-    if serializer.is_valid():
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
 
-    return Response(serializer.data)
+        return Response(serializer.data)
 
-@api_view(['GET'])
-def obtener_vacantes(request):
+class ObtenerVacanteView(APIView):
 
-    vacantes = Vacante.objects.all()
-    serializer = Vacante_Serializer(vacantes, many=True)
+    serializer_class = Vacante_Serializer
 
-    return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        pk = self.kwargs["pk"]
 
+        vacante = Vacante.objects.filter(id=pk)
+        serializer = Vacante_Serializer(vacante, many=True)
 
-@api_view(['GET'])
-def obtener_vacante(request, id):
+        return Response(serializer.data)
 
-    vacante = Vacante.objects.get(id=id)
-    serializer = Vacante_Serializer(vacante, many=True)
+class SolicitudesView(APIView):
 
-    return Response(serializer.data)
+    serializer_class = Solicitude_Serializer
 
-@api_view(['POST'])
-def crear_solicitud(request):
-
-    serializer = Solicitude_Serializer(data=request.data)
-
-    if serializer.is_valid():
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
 
-    return Response(serializer.data)
+        return Response(serializer.data)
+
+class CandidatoView(APIView):
+
+    serializer_class = Candidato_Serializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+class SolicitudesVacanteView(ApiView):
+
+    serializer_class = Solicitude_Serializer
+
+    def get(self, request, *args, **kwargs):
+
+        pk_vacante = self.kwargs['pk']
+
+        solicitudes = Solicitude.objects.filter(vacante=pk_vacante).order_by('-fecha')
+        serializer = self.serializer_class(solicitudes, many=True)
+
+        return Response(serializer.data)
+
+
+class VacantesEmpresaView(ApiView):
+
+    serializer_class = Vacante_Serializer
+
+    def get(self, request, *args, **kwargs):
+
+        pk_empresa = self.kwargs['pk']
+
+        vacantes = Vacante.objects.filter(empresa=pk_empresa).order_by('-fecha_hora')
+        serializer = self.serializer_class(vacantes, many=True)
+
+        return Response(serializer.data)
--- BackEnd/vacantes/models.py
+++ BackEnd/vacantes/models.py
@@ -25,7 +25,7 @@ class Categoria(models.Model):
 class Empresa(models.Model):
 
     nombre = models.CharField(max_length=50, blank=False, null=False)
-    dirección = models.CharField(max_length=90, blank=False, null=False)
+    direccion = models.CharField(max_length=90, blank=False, null=False)
     pais = models.CharField(max_length=30, blank=False, null=False)
     correo = models.EmailField(max_length=60, blank=False, null=False)
     correo_vacantes = models.EmailField(max_length=60, blank=False, null=False)
@@ -36,26 +36,27 @@ class Empresa(models.Model):
     url_facebook = models.URLField(max_length=100, blank=True, null=True)
     url_instagram = models.URLField(max_length=100, blank=True, null=True)
     url_twitter = models.URLField(max_length=100, blank=True, null=True)
-    Foto = models.ImageField(upload_to='vacantes/imagenes/empresas', blank=True, null=True)
+    foto = models.ImageField(upload_to='vacantes/imagenes/empresas', blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
 class Vacante(models.Model):
+
     nombre_puesto = models.CharField(max_length=50, blank=False, null=False)
-    categoría = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
     tipo_trabajo = models.CharField(max_length=10, choices=tipo_trabajo_opciones, default='TEMPORAL')
     forma_trabajo = models.CharField(max_length=10, choices=forma_trabajo_opciones, default='PRESENCIAL')
     experiencia = models.BooleanField(blank=False, null=False)
     responsabilidades_puesto = models.CharField(max_length=100, blank=True, null=True)
     requisitos_obligatorios = models.CharField(max_length=200, blank=False, null=False)
-    requisitos_obcionales = models.CharField(max_length=200, blank=False, null=False)
+    requisitos_opcionales = models.CharField(max_length=200, blank=False, null=False)
     salario_min = models.IntegerField(blank=False, null=False)
     salario_max = models.IntegerField(blank=False, null=False)
     beneficios = models.CharField(max_length=100, blank=True, null=True)
     horario_trabajo = models.CharField(max_length=25, blank=True, null=True)
-    fecha_hora = models.DateTimeField()
+    fecha_hora = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.nombre_puesto} - {self.empresa}"
