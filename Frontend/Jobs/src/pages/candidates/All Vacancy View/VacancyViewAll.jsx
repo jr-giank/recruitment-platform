a87b612d35b9dc2fdc8f0fbf55c5e192d5251d58
johannytavarez <scarlet.tavarezg@gmail.com>
2022-10-10 19:45:53 -0400

    Conexiones del frontend al backend

--- Frontend/Jobs/src/pages/candidates/All Vacancy View/VacancyViewAll.jsx
+++ Frontend/Jobs/src/pages/candidates/All Vacancy View/VacancyViewAll.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState}from 'react'
+import { get } from '../../../services/services'
 import VacancyGridAll from './components/VacancyGridAll'
 
 const VacancyViewAll = () => {
+
+    const [ vacancies, setVacancies ] = useState([])
+    
+    useEffect(() => {
+      get('vacantes/')
+      .then(data=> {
+        setVacancies(data)
+      })
+    }, [])
+
     return(
         <div className='w-full pt-2 mt-20 px-8'>
             <h1 className='text-center mt-10 font-bold text-2xl'>Vacantes disponibles</h1>
-            <VacancyGridAll/>
-            <VacancyGridAll/>
-            <VacancyGridAll/>
-            <VacancyGridAll/>
-            <VacancyGridAll/>
+            {
+          vacancies.map(vacancy => (
+            <VacancyGridAll key={vacancy.id} vacancy={vacancy} />
+          ))
+        }
         </div>
     )
 }
--- Frontend/Jobs/src/pages/candidates/All Vacancy View/components/VacancyGridAll.jsx
+++ Frontend/Jobs/src/pages/candidates/All Vacancy View/components/VacancyGridAll.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 
-const VacancyGridAll = () => {
+const VacancyGridAll = ({vacancy}) => {
    const history = useNavigate()
    Link
   return (
+
+    //poner las variables que vienen de vacancy, pais ni fecha esta disponible, categoria viene en numero
 
     <div className='w-full border-solid border-2 border-fifth rounded-md mt-5 mb-5 pl-4 pr-4 py-4 '>
 
@@ -12,9 +14,9 @@ const VacancyGridAll = () => {
             <img className='w-24 ' src="https://i.pinimg.com/originals/82/48/3b/82483b829d1a39580360a6fef506072d.png" alt="Scopic Software" />
         
             <div>
-                <h4 className='font-bold text-xl'>Remote Technical Lead             
-                    <span className='border-solid border-2 border-nineth rounded bg-nineth m-2 text-sm'>Independiente</span>
-                    <span className='border-solid border-2 border-nineth rounded bg-nineth m-2 text-sm'>Remoto</span>
+                <h4 className='font-bold text-xl'>{vacancy.nombre_puesto}             
+                    <span className='border-solid border-2 border-nineth rounded bg-nineth m-2 text-sm'>{vacancy.tipo_trabajo}</span>
+                    <span className='border-solid border-2 border-nineth rounded bg-nineth m-2 text-sm'>{vacancy.forma_trabajo}</span>
                 </h4>
                 <p className='text-base'>Scopic Software</p>
                 <ul className='flex space-x-4'>
@@ -25,7 +27,7 @@ const VacancyGridAll = () => {
             </div>
         </div>
 
-        <Link className='flex justify-center cursor-pointer w-full mt-5 p-2 hover:bg-secondary border-solid border-2 border-eighth rounded' to='/candidates/viewSingleVacancy' >Ver vacante</Link>  
+        <Link className='flex justify-center cursor-pointer w-full mt-5 p-2 hover:bg-secondary border-solid border-2 border-eighth rounded' to= {`/candidates/viewSingleVacancy/${vacancy.id}`} >Ver vacante</Link>  
     </div>
 
     
