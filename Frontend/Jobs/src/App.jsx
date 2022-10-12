dad7ecebe15a56e8d1d6d508155ad6ef345b76d3
Bryan <bryanmontero81@gmail.com>
2022-10-12 00:53:54 -0400

    Loader implementado

--- Frontend/Jobs/src/App.jsx
+++ Frontend/Jobs/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import AppRouter from './routes/AppRouter'
 
 const App = () => {
+ 
   return (
-    <AppRouter />
+      <AppRouter />
   )
 }
 
--- Frontend/Jobs/src/routes/AppRouter.jsx
+++ Frontend/Jobs/src/routes/AppRouter.jsx
@@ -1,30 +1,28 @@
 import React from 'react'
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+
 import CompanyRegistrationPage from '../pages/recruiters/Company Registration/CompanyRegistrationPage'
 import VacancyRegistrationPage from '../pages/recruiters/Vacancy Registration/VacancyRegistrationPage'
 import VacancyViewPage from '../pages/recruiters/Vacancy View/VacancyViewPage'
 import VacancyViewAll from '../pages/candidates/All Vacancy View/VacancyViewAll'
 import VacancyViewSingle from '../pages/candidates/Single Vacancy View/VacancyViewSingle'
-
-import Navigation from '../sharedComponents/navigation/navigation/Navigation'
+import Navigation from '../sharedComponents/navigation/Navigation'
 
 const AppRouter = () => {
+
   return (
     <>
-      <Router>
-        {/* <div className='h-screen overflow-hidden'> */}
-          <Navigation />
-          <Routes>
-              <Route path='/'  element={<VacancyViewPage /> } />
-              <Route path='/recruiter/signUp' element={<CompanyRegistrationPage />} />
-              <Route path='/recruiter/createVacancy' element={<VacancyRegistrationPage />} />
-              <Route path='/recruiter/viewVacancies' element={<VacancyViewPage />} />
-              <Route path='/candidates/viewAllVacancies' element={<VacancyViewAll />} />
-              <Route path='/candidates/viewSingleVacancy/:id' element={<VacancyViewSingle />} />
-          </Routes>
-
-        {/* </div> */}
-      </Router>
+      <Navigation />
+        <Router>
+            <Routes>
+                <Route path='/'  element={<VacancyViewPage /> } />
+                <Route path='/recruiter/signUp' element={<CompanyRegistrationPage />} />
+                <Route path='/recruiter/createVacancy' element={<VacancyRegistrationPage />} />
+                <Route path='/recruiter/viewVacancies' element={<VacancyViewPage />} />
+                <Route path='/candidates/viewAllVacancies' element={<VacancyViewAll />} />
+                <Route path='/candidates/viewSingleVacancy/:id' element={<VacancyViewSingle />} />
+            </Routes>
+        </Router>
     </>
   )
 }
