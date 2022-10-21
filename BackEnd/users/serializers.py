05eeea14b92dae59fff36c044fcc56099a56bce3
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-10-21 14:19:47 -0400

    Changing fields and deleteting fields of users

--- BackEnd/users/serializers.py
+++ BackEnd/users/serializers.py
@@ -7,12 +7,16 @@ class UserSerializer(serializers.ModelSerializer):
 
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True)
+<<<<<<< HEAD
     # username = serializers.CharField(required=True)
+=======
+>>>>>>> ff67184 (Changing fields and deleteting fields of users)
     is_staff = serializers.BooleanField(default=False)
 
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'is_staff']
+<<<<<<< HEAD
 
     def validate_password(self, value):
         return make_password(value)
@@ -26,6 +30,8 @@ class EmpresaSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'is_staff']
+=======
+>>>>>>> ff67184 (Changing fields and deleteting fields of users)
 
     def validate_password(self, value):
         return make_password(value)
