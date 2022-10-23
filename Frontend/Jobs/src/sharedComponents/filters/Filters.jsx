d3d3b6f0515a8bf0cc8673477dc5266577193d57
Bryan <bryanmontero81@gmail.com>
2022-10-23 19:37:42 -0400

    Trabajando en la implementacion de la funcionalidad de los filtros

--- Frontend/Jobs/src/sharedComponents/filters/Filters.jsx
+++ Frontend/Jobs/src/sharedComponents/filters/Filters.jsx
@@ -1,11 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { getCountries } from '../../services/services'
 import FiltersWithCheckBox from './FiltersWithCheckBox'
 import FilterWithRButton from './FilterWithRButton'
 
+const filtersInitialState = {
+    category: [], 
+    modalidad: [], 
+    exp: "No", 
+    job_type:[], 
+    countries:[]
+}
+
 const Filters = () => {
 
-    const [ countries, setCountries ] = useState([]) 
+    const [ countries, setCountries ] = useState([])
+    const [ openFilter, setOpenFilter ] = useState(null)
+    const firstRendered = useRef(false)
+    const displayedFilter = useRef(false)
+
+    const [ filters, setFilters ] = useState({...filtersInitialState})
 
     useEffect(() => {
         // Peticion al servidor para traer los países
@@ -15,13 +28,53 @@ const Filters = () => {
         })
       }, [])
 
+    useEffect(()=>{
+        console.log(filters)
+    },[filters])
+
+    // AGREGAR UN LISTENER PARA ESCUCHAR CUANDO SE LE DA UN CLICK A UN ELEMENTO FUERA DEL FILTRO QUE ESTA ABIERTO
+    useEffect(()=> {
+
+        if(!firstRendered.current){
+            firstRendered.current = true
+            return
+        } 
+
+        if(openFilter === null){
+            return
+        }
+
+        const filterOutsideClick = (e) => {
+            if(displayedFilter.current && !document.getElementById(`${openFilter}`).contains(e.target)){
+                document.getElementById(`${openFilter}`).classList.add("hidden")
+                displayedFilter.current = false
+                setOpenFilter(null)
+                window.removeEventListener("click", filterOutsideClick) // Remover listener
+                return
+            }
+            displayedFilter.current = true
+        }
+        window.addEventListener("click", filterOutsideClick)
+    }, [openFilter])
+
     const handleShowFilter = (id) => {
         document.getElementById(`${id}`).classList.toggle("hidden")
+        setOpenFilter(id)
+    }
+
+    // Limpiar filtros y limpiar elementos checks
+    const handleOnCleanFilters = () => {
+
+        setFilters({...filtersInitialState})
+        const checks = document.querySelectorAll(".checkbox_filter, .radio_filter")
+
+        Array.prototype.map.call(checks, element => {
+            element.checked = false    
+        });
     }
 
     return (
         <>
-            {/* <div> */}
                 <button 
                     className='rounded-lg px-4 py-1 border border-fifth hover:bg-secondary hover:text-white'
                     onClick={()=>handleShowFilter("category_filter")}
@@ -30,9 +83,14 @@ const Filters = () => {
                 </button>
 
                 <span 
-                    className='hidden absolute bg-white top-32 left-48 rounded-lg shadow-md text-[15px] py-2 h-80 overflow-auto' id='category_filter'
+                    className='hidden absolute bg-white top-32 left-48 rounded-lg shadow-md text-[15px] py-2 h-80 overflow-auto' 
+                    id='category_filter'
                 >
-                    <FiltersWithCheckBox itemsList={["Desarrollo Web", "Desarrollo Móvil", "Inteligencia Artificial", "QA", "Desarrollo Frontend", "Desarrollo Backend", "Desarrollo Blockchain", "Desarrollo de VideoJuegos", "Ciencia de Datos"]} />
+                    <FiltersWithCheckBox 
+                        itemsList={["Desarrollo Web", "Desarrollo Móvil", "Inteligencia Artificial", "QA", "Desarrollo Frontend", "Desarrollo Backend", "Desarrollo Blockchain", "Desarrollo de VideoJuegos", "Ciencia de Datos"]} 
+                        setFilters={setFilters} 
+                        filters={filters} 
+                        target={"category"} />
                 </span>
 
                 <button
@@ -43,7 +101,12 @@ const Filters = () => {
                 </button>
 
                 <span className='hidden absolute bg-white top-32 left-96 rounded-lg shadow-md text-[15px] py-2' id='modalidad_filter'>
-                    <FiltersWithCheckBox itemsList={["Presencial", "Híbrido", "Remoto"]} />
+                    <FiltersWithCheckBox    
+                        itemsList={["Presencial", "Híbrido", "Remoto"]} 
+                        setFilters={setFilters} 
+                        filters={filters} 
+                        target={"modalidad"} 
+                    />
                 </span>
 
                 <button
@@ -54,7 +117,12 @@ const Filters = () => {
                 </button>
 
                 <span className='hidden absolute bg-white top-32 left-[545px] rounded-lg shadow-md py-2' id='exp_filter'>
-                    <FilterWithRButton itemsList={["Si", "No"]} />
+                    <FilterWithRButton 
+                        itemsList={["Si", "No"]} 
+                        setFilters={setFilters} 
+                        filters={filters} 
+                        target={"exp"} 
+                    />
                 </span>
 
                 <button
@@ -65,7 +133,12 @@ const Filters = () => {
                 </button>
 
                 <span className='hidden absolute bg-white top-32 left-[630px] rounded-lg shadow-md text-[15px] py-2' id='jobType_filter'>
-                    <FiltersWithCheckBox itemsList={["Contrato por tiempo Indefinido", "Contrato Temporal", "Contrato a Medio Tiempo"]} />
+                    <FiltersWithCheckBox 
+                        itemsList={["Contrato por tiempo Indefinido", "Contrato Temporal", "Contrato a Medio Tiempo"]} 
+                        setFilters={setFilters} 
+                        filters={filters} 
+                        target={"job_type"} 
+                    />
                 </span>
 
                 <button
@@ -76,9 +149,27 @@ const Filters = () => {
                 </button>
 
                 <span className='hidden absolute bg-white top-32 left-[700px] rounded-lg shadow-md text-[14px] h-80 overflow-auto py-2' id='country_filter'>
-                    <FiltersWithCheckBox itemsList={countries} />
+                    <FiltersWithCheckBox 
+                        itemsList={countries} 
+                        setFilters={setFilters} 
+                        filters={filters} 
+                        target={"countries"} 
+                    />
                 </span>
-            {/* </div> */}
+
+                <span className='border-l border-sixth px-2'>
+                    <button 
+                        className='px-3 py-1 bg-seventh text-white rounded-md ml-2'
+                    >
+                        Aplicar Filtros
+                    </button>
+                    <button 
+                        className='px-3 py-1 bg-fourth text-white rounded-md ml-2' 
+                        onClick={handleOnCleanFilters}
+                    >
+                        Limpiar
+                    </button>
+                </span>
         </>
   )
 }
--- Frontend/Jobs/src/sharedComponents/filters/FiltersWithCheckBox.jsx
+++ Frontend/Jobs/src/sharedComponents/filters/FiltersWithCheckBox.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 
-const FiltersWithCheckBox = ({itemsList}) => {
+/*
+  target: el nombre de la lista de filtros a la cual hara referencia este componente.
+  itemsList: la lista de items que contiene el filtro.
+*/
+
+const FiltersWithCheckBox = ({itemsList, setFilters, filters, target}) => {
+
+  const handleOnCheck = (e) => {
+
+    // Agregar un elemento al filtrado siempre y cuando el elemento este checked
+    if(e.target.checked){
+      setFilters({...filters,  [target]:[...filters[target], e.target.value]})
+      return
+    }
+    
+    // En caso de querer eliminar un elemento del filtrado
+    const removedItemFilter = filters[target].filter(item => item !== e.target.value)
+    setFilters({...filters, [target]: [...removedItemFilter]})
+  }
 
   return (
     <div className='flex flex-col'>
         {
             itemsList.map(item => (
                 <div key={item} className='flex items-center px-3'>
-                    <input type="checkbox" name={item} id={`${item}`} value={item} className='checkbox_filter' />
+                    <input type="checkbox" name={item} id={`${item}`} value={item} className='checkbox_filter' onChange={handleOnCheck} />
                     <label htmlFor={`${item}`} className='ml-4 mt-3'>{item}</label>
                 </div>
             ))
