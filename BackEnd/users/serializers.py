e514a50c2114f984927b8eddb6a2b4c4a7d3bf41
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-10-24 23:12:19 -0400

    Updating serializer

--- BackEnd/users/serializers.py
+++ BackEnd/users/serializers.py
@@ -7,31 +7,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True)
-<<<<<<< HEAD
-    # username = serializers.CharField(required=True)
-=======
->>>>>>> ff67184 (Changing fields and deleteting fields of users)
     is_staff = serializers.BooleanField(default=False)
 
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'is_staff']
-<<<<<<< HEAD
-
-    def validate_password(self, value):
-        return make_password(value)
-
-class EmpresaSerializer(serializers.ModelSerializer):
-
-    email = serializers.EmailField(required=True)
-    password = serializers.CharField(required=True)
-    is_staff = serializers.BooleanField(default=False)
-
-    class Meta:
-        model = get_user_model()
-        fields = ['email', 'password', 'is_staff']
-=======
->>>>>>> ff67184 (Changing fields and deleteting fields of users)
 
     def validate_password(self, value):
         return make_password(value)
