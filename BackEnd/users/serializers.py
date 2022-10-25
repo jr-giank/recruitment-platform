29e4b12ed627553c769553f44fa98d7b512454ca
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-10-24 22:15:03 -0400

    Adding register and login token logic

--- BackEnd/users/serializers.py
+++ BackEnd/users/serializers.py
@@ -7,6 +7,20 @@ class UserSerializer(serializers.ModelSerializer):
 
     email = serializers.EmailField(required=True)
     password = serializers.CharField(required=True)
+    # username = serializers.CharField(required=True)
+    is_staff = serializers.BooleanField(default=False)
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'is_staff']
+
+    def validate_password(self, value):
+        return make_password(value)
+
+class EmpresaSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
     is_staff = serializers.BooleanField(default=False)
 
     class Meta:
