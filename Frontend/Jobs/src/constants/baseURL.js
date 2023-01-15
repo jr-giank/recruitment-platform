522ced327d1f69749cdd6067faa26af663415b18
Bryan <bryanmontero81@gmail.com>
2023-01-15 18:38:55 -0400

    .env

--- Frontend/Jobs/src/constants/baseURL.js
+++ Frontend/Jobs/src/constants/baseURL.js
@@ -1,9 +1,2 @@
-
-// for development
-export const BASE_URL = 'http://127.0.0.1:8000/api/'
-export const BASE_URL_FILES = 'http://127.0.0.1:8000'
-
-// Production
-// export const BASE_URL = "https://vacantes-backend-production.up.railway.app/api/"
-// export const BASE_URL_FILES = "https://vacantes-backend-production.up.railway.app"
-
+export const BASE_URL = import.meta.env.VITE_BASE_URL
+export const BASE_URL_FILES = import.meta.env.VITE_BASE_URL_FILES
